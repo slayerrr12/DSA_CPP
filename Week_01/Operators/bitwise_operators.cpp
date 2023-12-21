f494b55ace605898f9c6469f9add09b0ae77b99d
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-21 13:21:09 +0530

    week one start

---
+++ Week_01/Operators/bitwise_operators.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * The main function is the entry point of the program.
+ * It demonstrates the usage of bitwise operators.
+ */
+int main()
+{
+
+    int  a = 3;
+    int b = 4;
+
+    cout << (a&b) << endl; // 2
+    cout << (a | b )<< endl ; //3
+    cout << (a ^ b) << endl; //1
+
+
+
+
+
+
+
+
+return 0;
+}

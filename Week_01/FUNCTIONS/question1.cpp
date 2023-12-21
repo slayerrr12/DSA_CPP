a93c26c35f1eefac49c4796d60d1e922c1c91a86
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-21 16:23:41 +0530

    checkEvenodd suing bitwise

---
+++ Week_01/FUNCTIONS/question1.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// using normal method
+bool checkEven(int n)
+{
+    if (n % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool CheckEvenBitwise(int n)
+{
+    if (n & 1 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    // if (checkEven(n))
+    // {
+    //     cout << "even" << endl;
+    // }
+    // else
+    // {
+    //     cout << " odd" << endl;
+    // }
+    // if (CheckEvenBitwise(n))
+    // {
+    //     cout << "even" << endl;
+    // }
+    // else
+    // {
+    //     cout << " odd" << endl;
+    // }
+
+    return 0;
+}

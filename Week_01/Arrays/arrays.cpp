67bcd4fd01696a018c3e5cd6e3cc2f07dcddc393
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-22 16:10:17 +0530

    what the hell is this

--- Week_01/Arrays/arrays.cpp
+++ Week_01/Arrays/arrays.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 
 
+void updateArray( int arr[], int n , int index){
+
+    // here only the address of the first block is passed into arr 
+    // arr is actually a pointer that contains the address of the first block of a 
+   
+    // so arr[index] = n is actually *(arr+index) = n    
+    arr[index] = n;
+    cout<<arr[index]<<endl;
+}
+
+
+void printArray(int arr[], int n){
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
 
 
 
@@ -17,6 +35,11 @@ int main()
 
     // a contains the adress of the the first block
     cout<<a<<endl;
+  printArray(a, 8);
+    updateArray(a, 1000000, 5);
+    printArray(a, 8);
+
+
   
 return 0;
 }

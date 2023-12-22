7e11b26bd91aa91fc4f56ab40c01556783c5ae24
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-22 16:02:54 +0530

    added the arrays section

---
+++ Week_01/Arrays/arrays.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+
+
+
+int main()
+{
+
+    int a[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    // to find size of array
+    cout << sizeof(a)/ sizeof(int) << endl;
+
+    // a contains the adress of the the first block
+    cout<<a<<endl;
+  
+return 0;
+}

88c38825e61755a084950a1ea82f66209911624e
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-24 23:32:54 +0530

    wth

--- LeetCode/Reverse_Integer.cpp
+++ LeetCode/Reverse_Integer.cpp
@@ -5,9 +5,15 @@ public:
          while (x!=0)
          {
             int digit = x%10;
+            if (ans > INT_MAX/10 || (ans < INT_MIN/10)
+            {
+                return 0;
+            }
+            
+            
             ans = (ans * 10 ) + digit;
             x = x/10;   
-            if 
+             
 
          }
             return ans;

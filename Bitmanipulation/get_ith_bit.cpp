9636e75bf923a61ca550404b364b37418c39bc0d
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-27 14:35:37 +0530

    get ith bit

--- Bitmanipulation/get_ith_bit.cpp
+++ Bitmanipulation/get_ith_bit.cpp
@@ -2,10 +2,24 @@
 
 using namespace std;
 
-int 
+int ith_bit(int n, int i)
+{
+    int ith_bit = (n & (1 << i));
+    if (ith_bit == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
 
 int main()
 {
+    int n = 222;
+    cout << ith_bit(n, 3) << endl;
+
 
     return 0;
 }

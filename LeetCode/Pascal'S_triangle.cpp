841518b81280d49cc8e7b03c8a232d342ba61715
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-30 00:13:40 +0530

    done

---
+++ LeetCode/Pascal'S_triangle.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> result;
+        a.reserve(numRows);
+        for(int i = 0 ; i < numRows ; i++){
+            vector<int> row(i+1,1);
+            
+            for (int j = 1; j < i; j++)
+            {
+                  row[j] = result[i - 1][j - 1] + result[i - 1][j];
+                  
+                
+            
+                
+            }
+            
+
+
+
+        }
+
+    }
+};

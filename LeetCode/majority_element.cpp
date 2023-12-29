c91cda95fe6c63ba75d094d833926cdef649fa43
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-30 00:47:25 +0530

    another problem

---
+++ LeetCode/majority_element.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        unordered_map<int, int> Frequency_count;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            Frequency_count[nums[i]]++;
+        }
+        for (auto &pair : Frequency_count)
+        {
+            if (pair.second >= nums.size() / 2)
+            {
+                return pair.first;
+            }
+        }
+    }
+}

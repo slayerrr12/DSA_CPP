06c78c5a16cc6fd62dd0aae0722342526ba404c8
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-31 23:17:42 +0530

    update the code of reverse bits

--- LeetCode/Reverse_Bits.cpp
+++ LeetCode/Reverse_Bits.cpp
@@ -1,33 +1,32 @@
-// difficulty Easy
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-
-using namespace std;
-
 class Solution
 {
 public:
     uint32_t reverseBits(uint32_t n)
     {
+        // Initialize the result to store the reversed bits.
+        uint32_t ans = 0;
 
-        int ans = 0;
-        int count = 0;
-        int ith_bit = 0;
-        for (int i = 31; i > -1; i++)
+        // Counter variable to keep track of the position of the reversed bit.
+        int j = 0;
 
+        // Loop through each bit of the input number from the most significant bit (31) to the least significant bit (0).
+        for (int i = 31; i >= 0; i--)
         {
-            ith_bit = n & (1 << i);
-            if (ith_bit == 0)
+            // Extract the i-th bit from the input number.
+            int ith_bit = n & (1 << i);
+
+            // Check if the i-th bit is 1 (not 0).
+            if (ith_bit != 0)
             {
-                continue;
+                // If the i-th bit is 1, set the corresponding bit in the result using bitwise left shift.
+                ans |= (1u << j);
             }
-            else
-            {
-                ans += pow(2, i);
-            }
+
+            // Increment the counter variable to move to the next position for the reversed bit.
+            j++;
         }
+
+        // Return the final result with reversed bits.
+        return ans;
     }
 };

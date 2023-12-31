b7847ff6a86c40ef06165ec13d2e6ab3e0d9e8bd
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-31 19:16:41 +0530

    no. of ones

---
+++ LeetCode/no_of_one_bits.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+
+        //using bit masking and also checking each bit
+        for (int i = 0; i < ; i++)
+        {
+            /* code */
+        }
+        
+        
+    }
+};

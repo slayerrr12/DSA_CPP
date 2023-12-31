de61d15cda423b8fcb9909c3ebe0b306cc0231a8
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-31 19:49:22 +0530

    missing number cpp

---
+++ LeetCode/Missing_number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans =0;
+        if(nums[nums.size()-1]!=nums.size()){
+            return nums.size();
+        }
+        if(nums[0]!=0){
+            return 0;
+        }
+        for(int i ; i < nums.size(); i++){
+
+            if(nums[i]!=i){
+                ans = i;
+                return i;
+            }
+
+           
+            
+        }
+        return ans;
+    }
+};
--- LeetCode/no_of_one_bits.cpp
+++ LeetCode/no_of_one_bits.cpp
@@ -4,13 +4,17 @@ using namespace std;
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-
+        int count = 0;
         //using bit masking and also checking each bit
-        for (int i = 0; i < ; i++)
+        for (int i = 0; i < 31; i++)
+
         {
-            /* code */
+            if((n&(1<<i))!=0){
+                count++;
+            }
+          
         }
-        
+        return count;
         
     }
 };

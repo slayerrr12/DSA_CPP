b3489369d0dd62ff864ccbdd692caa9e5f340db1
slayerrr12 <eyeamarinsharma@gmail.com>
2023-12-31 17:35:43 +0530

    another easy problem solved

---
+++ LeetCode/Defanging_IP_ADRESS.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        int count = 0;
+        for(int i =0 ; i < address.size(); i++){
+           if(count==5){
+                break;
+              
+           }
+            if(address[i] == '.'){
+                address.replace(i, 1, "[.]");
+                i+=2;
+                count++;
+            }
+
+
+    }
+
+    return address;
+    }
+};

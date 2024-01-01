bf4d5bb0533354dda0392e73d39488b5c528d8ad
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-01 17:43:04 +0530

    two pointers approach

---
+++ TwoPointerQuestions/Sort_0s_1s_array.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <map>
+
+
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt","r",stdin);
+freopen("output.txt","w",stdout);
+#endif
+
+return 0;
+}

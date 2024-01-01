3f786efe5c008fe9a41ba410f8facc1e15013974
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-01 17:52:48 +0530

    code added for sorting using two pointers approach

--- TwoPointerQuestions/Sort_0s_1s_array.cpp
+++ TwoPointerQuestions/Sort_0s_1s_array.cpp
@@ -7,20 +7,66 @@
 #include <unordered_set>
 #include <map>
 
-
-
 using namespace std;
 
+void sortZeroesAndOnes(vector<int> &nums)
+{
+    int n = nums.size();
+    int i = 0;
+    int j = n - 1;
+    while (i < j)
+    {
+        if (nums[i] == 0 && nums[j] == 1)
+        {
+            i++;
+            j--;
+            continue;
+        }
+        if (nums[i] == 1 && nums[j] == 1)
+        {
 
+            j--;
+            continue;
+        }
+        if (nums[i] == 1 && nums[j] == 0)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+            continue;
+        }
 
+        if (nums[i] == 0 && nums[j] == 0)
+        {
+            i++;
 
+            continue;
+        }
+    }
+}
+
+void printVector(vector<int> &nums)
+{
+    for (auto i : nums)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-freopen("input.txt","r",stdin);
-freopen("output.txt","w",stdout);
-#endif
 
-return 0;
+    vector<int> nums = {0, 1, 0, 1, 0, 0, 1, 1, 1, 0};
+    sortZeroesAndOnes(nums);
+    cout << "Sorted array is: "<< endl;
+
+    printVector(nums);
+    
+
+
+
+    return 0;
 }

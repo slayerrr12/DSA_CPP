0a3d9f75d21e21e917536ac78f8d436b6b96b4c3
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-01 18:12:11 +0530

    better commented out version of code

--- TwoPointerQuestions/EvenLeftSide_OddRightSide.cpp
+++ TwoPointerQuestions/EvenLeftSide_OddRightSide.cpp
@@ -9,29 +9,39 @@
 
 using namespace std;
 
+// Function to sort an array of integers containing both odd and even numbers
 void sortOddAndEvens(vector<int> &nums)
 {
     int n = nums.size();
-    int i = 0;
-    int j = n - 1;
+    int i = 0; // Initialize the left pointer
+    int j = n - 1; // Initialize the right pointer
+
+    // Continue sorting until the left pointer crosses the right pointer
     while (i < j)
     {
+        // Check if both elements are even, move the left pointer to the right
         if ((nums[i] & 1) == 0 && (nums[j] & 1) == 0)
         {
             i++;
             continue;
         }
+
+        // Check if both elements are odd, move the right pointer to the left
         if ((nums[i] & 1) != 0 && (nums[j] & 1) != 0)
         {
             j--;
             continue;
         }
+
+        // Swap if the left element is even and the right element is odd
         if ((nums[i] & 1) == 0 && (nums[j] & 1) != 0)
         {
             i++;
             j--;
             continue;
         }
+
+        // Swap if the left element is odd and the right element is even
         if ((nums[i] & 1) != 0 && (nums[j] & 1) == 0)
         {
             int temp = nums[i];
@@ -44,6 +54,7 @@ void sortOddAndEvens(vector<int> &nums)
     }
 }
 
+// Function to print the elements of a vector
 void printVector(vector<int> &nums)
 {
     for (auto i : nums)
@@ -53,13 +64,17 @@ void printVector(vector<int> &nums)
     cout << endl;
 }
 
+// Main function
 int main()
 {
-
+    // Example vector of integers
     vector<int> nums = {0, 13, 0, 1, 0, 123441, 1, 1, 1, 0};
-    sortOddAndEvens(nums);
-    cout << "Sorted array is: " << endl;
 
+    // Call the function to sort the array
+    sortOddAndEvens(nums);
+
+    // Print the sorted array
+    cout << "Sorted array is: " << endl;
     printVector(nums);
 
     return 0;

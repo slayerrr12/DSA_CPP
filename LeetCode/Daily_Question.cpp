a14e7ef9346b13c8c6d81f765204fd55c6b651f8
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-02 16:17:56 +0530

    daily question Leetcode

---
+++ LeetCode/Daily_Question.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end())
+        sort(s.begin(),g,end())
+        int i = 0 ;
+        int j = 0 ;
+        
+
+
+    }
+};

45297b5b6383032c153608920c082f4e517d377f
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-13 14:37:03 +0530

    solved

--- LeetCode/Shuffle_the_Array.cpp
+++ LeetCode/Shuffle_the_Array.cpp
@@ -1,9 +1,32 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+
+using namespace std;
+
+
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-         
-         
+        n = nums.size();
+        vector<int> result;
 
+
+        for (int i = 0; i < n/2; i++)
+        {
+            result.push_back(nums[i]);
+            result.push_back(nums[i+n/2]);
+            
+        }
+
+        return result;
+        
+         
+         
+ 
         
     }
 };

384d9083814773288c43397178f71e988354e196
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-14 17:14:09 +0530

    code pushing find all subsets in bit manipulation segment of the repo

--- Bitmanipulation/Find_all_subsequences.cpp
+++ Bitmanipulation/Find_all_subsequences.cpp
@@ -1,34 +1,42 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#include <string.h>
-
+#include<iostream>
+#include<cstring>
 using namespace std;
 
+// Function to print characters based on binary representation of a number
+void overLaynumber(char arr[], int number) {
+    int j = 0;
 
-
-void overLay
-
-void generateAllSUbsequneces(char *arr){
-    int n = strlen(arr);
-
-    for(int i = 0 ; i <(i<<n) ; i++){
-        for (int j = 0; j < n; j++)
-        {
-            if((i & (1<<j)) != 0){
-                cout << arr[j];
-            }
+    while (number > 0) {
+        int last_bit = number & 1;
+        if (last_bit) {
+            cout << arr[j];
         }
+
+        j++;
+        number = number >> 1;
     }
+    cout << endl;
 }
 
+// Function to generate all subsequences of a given character array
+void generateAllSubsequences(char arr[]) {
+    int n = strlen(arr);
 
+    // Iterate through all possible values of i using bitmask
+    for (int i = 0; i < (1 << n); i++) {
+        // Print characters based on the current bitmask
+        overLaynumber(arr, i);
+    }
+    return;
+}
 
-int main()
-{
-
+int main() {
+    // Subsets / Subsequences
     char arr[10000];
+    // Input the character array from the user
     cin >> arr;
+    // Generate and print all subsequences
+    generateAllSubsequences(arr);
 
-
-return 0;
+    return 0;
 }

87f64d37ae2dd570dee48e54154c99038c4f4f67
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-15 22:56:43 +0530

    co pilot docs

---
+++ LeetCode/Concatenation_of_array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+    * Concatenates the given vector with itself.
+    * 
+    * @param nums The vector to be concatenated.
+    * @return The concatenated vector.
+    */
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+    }
+};

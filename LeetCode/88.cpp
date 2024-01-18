799fc211259d49a0c4753fa3e771590eac045b46
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-18 14:13:13 +0530

    commented out code with better readability

--- LeetCode/88.cpp
+++ LeetCode/88.cpp
@@ -2,48 +2,61 @@
 #include <vector>
 
 using namespace std;
-class Solution
-{
-public:
-    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
-    {
 
+class Solution {
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+        // Create a temporary vector to store the merged result
         vector<int> ans;
+
+        // Pointers for iterating through nums1 and nums2
         int i = 0;
         int j = 0;
 
-        while ((n > j && (m > i)))
-        {
-            if (nums1[i] >= nums2[j])
-            {
+        // Merge the two sorted arrays
+        while ((n > j && (m > i))) {
+            if (nums1[i] >= nums2[j]) {
+                // If current element in nums1 is greater or equal, add nums2[j] to the result
                 ans.push_back(nums2[j]);
                 j++;
-            }
-
-            else
-            {
+            } else {
+                // If current element in nums1 is smaller, add nums1[i] to the result
                 ans.push_back(nums1[i]);
                 i++;
-
-                continue;
             }
         }
 
-        while (i < m)
-        {
+        // Add remaining elements from nums1, if any
+        while (i < m) {
             ans.push_back(nums1[i]);
             i++;
         }
 
-        while (j < n)
-        {
+        // Add remaining elements from nums2, if any
+        while (j < n) {
             ans.push_back(nums2[j]);
             j++;
         }
 
-        for (int i = 0; i < ans.size(); i++)
-        {
+        // Copy the merged result back to nums1
+        for (int i = 0; i < ans.size(); i++) {
             nums1[i] = ans[i];
         }
     }
 };
+
+int main() {
+    // Example usage of the merge function
+    Solution sol;
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    vector<int> nums2 = {2, 5, 6};
+    int m = 3, n = 3;
+    sol.merge(nums1, m, nums2, n);
+
+    // Print the merged array
+    for (int num : nums1) {
+        cout << num << " ";
+    }
+
+    return 0;
+}

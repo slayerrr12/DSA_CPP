3a7c810c3399057087a8bdfd34c7767538d46de9
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-19 19:30:08 +0530

    instant swap function

--- LeetCode/189.cpp
+++ LeetCode/189.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void rotate(vector<int>& nums, int k) {
-        
+    void swap(int &a, int &b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    void rotate(vector<int> &nums, int k)
+    {
+        if (nums.size() < k)
+
+        {
+            k = nums.size() - k % nums.size();
+        }
+
+        int j = nums.size();
+        int i = j - k;
+        int l = j - k;
+
+        while (j >= l)
+        {
+        }
     }
 };

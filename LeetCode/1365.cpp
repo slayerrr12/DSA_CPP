9853c8054d89a25cfa04df98109c527176ed366d
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-20 23:33:22 +0530

    1365

---
+++ LeetCode/1365.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+      for (int i = 0; i < nums.size(); i++)
+      {
+        
+      }
+      
+       
+        
+    }
+};

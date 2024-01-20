ddb02fdb3193a99a73904c303703474a5d9aa71e
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-20 19:36:54 +0530

    playing with pointers

--- test_file.cpp
+++ test_file.cpp
@@ -2,26 +2,22 @@
 #include <stack>
 #include <string>
 
+using namespace std;
 
-using namespace std ;
-
-
-
-
-int main() {
+int main()
+{
 
     int *ptr = NULL;
-    cout<<"value of p when pointed to null : "<<ptr<<endl;
+    cout << "value of p when pointed to null : " << ptr << endl;
     ptr = 0;
-    cout<<"value of p when pointed to 0 : "<<ptr<<endl;
+    cout << "value of p when pointed to 0 : " << ptr << endl;
 
     int a = 3;
     ptr = &a;
-    cout<<"address of a  : "<<ptr<< " and the value at that address : "<< *ptr<< endl;
-   
+    cout << "address of a  : " << ptr << " and the value at that address : " << *ptr << endl;
+    *(ptr + 1) = 34;
+    cout << " value of of ptr + 1 " << *(ptr + 1) << endl;
+    
 
-
-   
     return 0;
-   
 }

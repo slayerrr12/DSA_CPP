781023fa96e8841ce9e307a4cbd9d61f056e4e6c
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-20 22:48:24 +0530

    issorted check

--- Recursion/IsSorted.cpp
+++ Recursion/IsSorted.cpp
@@ -2,14 +2,31 @@
 
 using namespace std;
 
-bool isSorted(int *arr , int n ) {
-    
-
+bool isSorted(float *arr, int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return true;
+    }
+    if (arr[0] < arr[1])
+    {
+        return isSorted(arr + 1, n - 1);
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 int main()
 {
 
+    float arr[] = {1,2,3,4,5.999,6,7,8};
+    int n = 8;
 
-return 0;
+    cout<<isSorted(arr,n)<<endl;
+
+
+
+    return 0;
 }

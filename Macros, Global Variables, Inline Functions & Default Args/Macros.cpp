50bfcb76668d1e401dff00bebdbe4867ab0211e9
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-21 15:00:29 +0530

    nothing just a commit

---
+++ Macros, Global Variables, Inline Functions & Default Args/Macros.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#define PI_2 3.14*2
+
+using namespace std;
+
+int main()
+{
+    cout<<PI_2<<endl;
+
+    cout<<"area of a circle with radius 4 :"<<PI_2*(4*4)<<endl;
+
+
+return 0;
+}

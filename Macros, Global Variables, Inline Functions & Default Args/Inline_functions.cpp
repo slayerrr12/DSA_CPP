3f5e23f485edbf62632b15425c5385f543441389
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-21 15:18:06 +0530

    inline functions

---
+++ Macros, Global Variables, Inline Functions & Default Args/Inline_functions.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+ inline int getmax(int a, int b)
+{
+
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+int main()
+{
+
+    cout<<getmax(344, 13143);
+
+    return 2;
+}

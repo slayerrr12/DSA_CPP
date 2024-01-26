793a85750531b07d4ea19f51a452014dad53fbcb
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-26 22:51:50 +0530

    halfway into it

--- LeetCode/2095.cpp
+++ LeetCode/2095.cpp
@@ -10,15 +10,38 @@ struct ListNode
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-
-class Solution {
+class Solution
+{
 public:
-    ListNode* deleteMiddle(ListNode* head) {
+    ListNode *deleteMiddle(ListNode *head)
+    {
+
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        ListNode *prev = head;
+        ListNode 
+
+        int count = 0;
+
+        while (fast != NULL || fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            count++;
+        }
+        while (count--)
+        {
+            prev = prev->next;
+        }
 
         
 
 
-        
-        
+
+
+
+
+
     }
 };

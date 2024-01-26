fc2fd5b35752688ff9a453f2e829e104094a12ce
slayerrr12 <eyeamarinsharma@gmail.com>
2024-01-26 22:39:09 +0530

    delete the middle node

--- LeetCode/142.cpp
+++ LeetCode/142.cpp
@@ -18,17 +18,24 @@ public:
         ListNode *fast = head;
         ListNode *slow = head;
 
-        while  (fast != NULL && fast->next != NULL)
+        while (fast != NULL && fast->next != NULL)
 
         {
             fast = fast->next->next;
             slow = slow->next;
-            if (fast==slow)
+            if (fast == slow)
             {
-                return true;
+                ListNode *entry = head;
+                int count = 0;
+                while (entry != head)
+                {
+                    entry = entry->next;
+                    slow = slow->next;
+                    count++;
+                }
+                return count;
             }
             return false;
         }
-
     }
 };
